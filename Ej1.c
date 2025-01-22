7cce081665c74471dcc62d7be4c7d67e1e24fd79
Ruben Guillen <ruben.guillen.dosa@gmail.com>
2025-01-22 14:26:04 +0100

    asd

--- Ej1.c
+++ Ej1.c
@@ -1,32 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void orden(int * pArr, int numeros){
+void orden(int * pArr, int numeros, int contador, int numeroMayor, int intercambio){
+
+
 
 	for (int i = 0; i < numeros; ++i){
 		for (int j = 0; j < numeros; ++j){
 
-		if(pArr[i] > pArr[j+ 1]){
+			if(pArr[i] > pArr[j]){
 
-			printf("%d\n", pArr[i]);
+			//printf("1. compraracion entre %d y %d\n\n", pArr[j], pArr[i]);
+			//printf("2. valor antes del intercambio %d\n\n ", intercambio);
 
-		}else if(pArr[i +1 ] < pArr[j]){
+				pArr[j] = pArr[i];
 
-			printf("%d\n", pArr[j]);
+				intercambio = pArr[i];
+
+			//printf("3. valor despues del intercambio  ,%d\n\n",intercambio);
+
+
+			}
+
+
+
 		}
 
-
-		}
 
 	}
 
+						printf("4. valor final ,%d\n\n",intercambio);
+
+
 }
+
 
 int main(){
 
 int arr[100];
 int *pArr = arr;
 int numeros, contador = 0;
+int numeroMayor = 0;
+int intercambio = 0;
+
 
 
 printf("Introduce cuantos numeros quieres usar: \n");
@@ -39,7 +55,10 @@ scanf("%d", &arr[i]);
 
 }
 
-orden(pArr, numeros);
+orden(pArr, numeros, contador, numeroMayor, intercambio);
+
+
 
 return 0;
+
 }

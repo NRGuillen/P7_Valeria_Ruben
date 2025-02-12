56daf27e5f9d1ff20e3215760f09b2b6e0c11ae4
Hidalgo002 <vhidalgodominguez@gmail.com>
2025-02-12 10:33:32 +0100

    “Actualizacion”

--- Práctica8/P8_Valeria_Rubén/cazador.c
+++ Práctica8/P8_Valeria_Rubén/cazador.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    system("chafa blood.jpg"); // Guardas el archivo en la misma carpeta que el .c
+#include <colores.h>
 
+int main() {
+    system("chafa -f symbols -s 70x50 Caz2.jpg"); //PARA USAR CHAFA, EL USUARIO HA DE DESCARGARSE "sudo apt install chafa"
     printf("NOMBRE: Bardo.\n");
+    system("chafa -f symbols -s 70x50 blood.jpg");
+
+    printf("NOMBRE: Hunter.\n");
     return 0;
 }
